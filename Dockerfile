20b4f0e3cf0ea235d4dcbe64ab096d9c42eb6e92
Yarince Martis <yarince@robinhq.com>
2022-04-01 12:20:24 +0200

    prometheus works!

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN dotnet publish -o /app/published-app
 
 FROM mcr.microsoft.com/dotnet/aspnet:6.0-alpine as runtime
 EXPOSE 80
+EXPOSE 9464
 WORKDIR /app
 COPY --from=build /app/published-app /app
 ENTRYPOINT [ "dotnet", "/app/simple-container.dll" ]
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics.Metrics;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,4 +26,24 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+
+Meter MyMeter = new("MyCompany.MyProduct.MyLibrary", "1.0");
+Counter<long> MyFruitCounter = MyMeter.CreateCounter<long>("MyFruitCounter");
+
+
+using var meterProvider = Sdk.CreateMeterProviderBuilder()
+       .AddMeter("MyCompany.MyProduct.MyLibrary")
+        .AddPrometheusExporter(options => { options.StartHttpListener = true; })
+       .Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint(meterProvider);
+
+MyFruitCounter.Add(1, new("name", "apple"), new("color", "red"));
+MyFruitCounter.Add(2, new("name", "lemon"), new("color", "yellow"));
+MyFruitCounter.Add(1, new("name", "lemon"), new("color", "yellow"));
+MyFruitCounter.Add(2, new("name", "apple"), new("color", "green"));
+MyFruitCounter.Add(5, new("name", "apple"), new("color", "red"));
+MyFruitCounter.Add(4, new("name", "lemon"), new("color", "yellow"));
+
 app.Run();
+
